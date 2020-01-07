22d46ee5b7102b8af544d5d723281e57c06daf82
Stenzel <stenzel.punch@gmail.com>
2020-01-07 17:20:51 +0200

    Updates

---
+++ precache-manifest.367b933cf4c9727191d9671074a562ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ddb751e2046495b3057f14ed761fc97",
+    "url": "/wheel-it/index.html"
+  },
+  {
+    "revision": "f88a116e9971ea411615",
+    "url": "/wheel-it/static/css/main.0e4164f8.chunk.css"
+  },
+  {
+    "revision": "6bdedc571b260c94222b",
+    "url": "/wheel-it/static/js/2.2f168dbb.chunk.js"
+  },
+  {
+    "revision": "ec8e7a56ad098d483e5fa926a01163f3",
+    "url": "/wheel-it/static/js/2.2f168dbb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f88a116e9971ea411615",
+    "url": "/wheel-it/static/js/main.2de987ea.chunk.js"
+  },
+  {
+    "revision": "b05306e7b4805bb282eb",
+    "url": "/wheel-it/static/js/runtime-main.d0579257.js"
+  },
+  {
+    "revision": "c6a0c23749ad1bb71eb74320e73cdb6d",
+    "url": "/wheel-it/static/media/start_page_bg.c6a0c237.svg"
+  },
+  {
+    "revision": "17aed9436dbb8267dc8240d42277a07b",
+    "url": "/wheel-it/static/media/wheel_arrow.17aed943.svg"
+  }
+]);
